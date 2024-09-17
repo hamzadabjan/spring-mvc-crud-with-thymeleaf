a08d769315282072e43476e4bdade7f96b88c47a
Hamza <hamza.dabjan@gmail.com>
2024-09-18 00:37:16 +0300

    spring MVC CRUD with thymeleaf

---
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -0,0 +1,71 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping ("employees")
+public class EmployeeController {
+
+   private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService theEmployeeService) {
+        this.employeeService=theEmployeeService;
+    }
+
+    // add mapping for (/list)
+
+    @GetMapping("/list")
+    public String listEmployees (Model theModel){
+
+        List<Employee> theEmployees= employeeService.findAll();
+
+        theModel.addAttribute("employees", theEmployees);
+
+        return "list-employees";
+    }
+
+    // add mapping for (/addNewEmployee)
+    @GetMapping ("/addNewEmployee")
+    public String addNewEmployeeForm(Model theModel){
+
+        Employee theEmployee = new Employee();
+
+        theModel.addAttribute("employee",theEmployee);
+
+        return "EmployeeForm";
+
+    }
+
+    // add mapping for (/saveNewEmployeeProcess)
+    @PostMapping("/saveEmployeeProcess")
+    public String saveNewEmployeeProcess(@ModelAttribute("employee") Employee theEmployee){
+        employeeService.save(theEmployee);
+        return "redirect:/employees/list";
+    }
+
+    // add mapping for (/updateEmployeeForm)
+    @GetMapping ("/updateEmployeeForm")
+    public String updateEmployeeForm (@RequestParam("employeeId") int employeeId, Model model){
+
+        Employee tempEmployee = employeeService.findById(employeeId);
+
+        model.addAttribute("employee",tempEmployee);
+
+        return "EmployeeForm";
+    }
+
+    // add mapping for (/deleteEmployee)
+    @GetMapping ("/deleteEmployee")
+    public String deleteEmployee (@RequestParam("employeeId") int theId){
+
+        employeeService.deleteById(theId);
+        return "redirect:/employees/list";
+    }
+}
---
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -0,0 +1,21 @@
+package com.luv2code.springboot.thymeleafdemo.dao;
+
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+
+
+    // Add method to sort by first name
+    public List<Employee> findAllByOrderByFirstNameAsc();
+
+    // Find employees whose emails are from a specific domain
+    @Query("SELECT e FROM Employee e WHERE e.email LIKE %:emailDomain%")
+    List<Employee> findEmployeesByEmailDomain(@Param("emailDomain") String emailDomain);
+
+}
